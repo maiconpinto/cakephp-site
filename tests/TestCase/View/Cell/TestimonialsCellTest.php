17f6820ded215f6d31374bb94376bc6c04cd465e
Maicon Pinto <maiconsilva.pinto@gmail.com>
2018-03-20 19:55:30 -0300

    TestimoinalsCells

---
+++ tests/TestCase/View/Cell/TestimonialsCellTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Site\Test\TestCase\View\Cell;
+
+use Cake\TestSuite\TestCase;
+use Site\View\Cell\TestimonialsCell;
+
+/**
+ * Site\View\Cell\TestimonialsCell Test Case
+ */
+class TestimonialsCellTest extends TestCase
+{
+
+    /**
+     * Request mock
+     *
+     * @var \Cake\Http\ServerRequest|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $request;
+
+    /**
+     * Response mock
+     *
+     * @var \Cake\Http\Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $response;
+
+    /**
+     * Test subject
+     *
+     * @var \Site\View\Cell\TestimonialsCell
+     */
+    public $Testimonials;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->request = $this->getMockBuilder('Cake\Http\ServerRequest')->getMock();
+        $this->response = $this->getMockBuilder('Cake\Http\Response')->getMock();
+        $this->Testimonials = new TestimonialsCell($this->request, $this->response);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Testimonials);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test display method
+     *
+     * @return void
+     */
+    public function testDisplay()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
