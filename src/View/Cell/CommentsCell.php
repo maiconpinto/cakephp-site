71ae021d949ca45a3cae776ea297c7c4d2036708
Maicon Pinto <maiconsilva.pinto@gmail.com>
2018-03-23 06:51:02 -0300

    CommentsCell

---
+++ src/View/Cell/CommentsCell.php
@@ -0,0 +1,36 @@
+<?php
+namespace Site\View\Cell;
+
+use Cake\View\Cell;
+
+/**
+ * Comments cell
+ */
+class CommentsCell extends Cell
+{
+
+    /**
+     * List of valid options that can be passed into this
+     * cell's constructor.
+     *
+     * @var array
+     */
+    protected $_validCellOptions = [];
+
+    /**
+     * Default display method.
+     *
+     * @return void
+     */
+    public function display()
+    {
+    }
+
+    public function post($element, $post_id) {
+        $this->set('element', $element);
+
+        $this->loadModel('Site.Comments');
+        $comments = $this->Comments->find('all')->where(['post_id' => $post_id, 'status' => 1]);
+        $this->set('comments', $comments->all());
+    }
+}
---
+++ tests/TestCase/View/Cell/CommentsCellTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Site\Test\TestCase\View\Cell;
+
+use Cake\TestSuite\TestCase;
+use Site\View\Cell\CommentsCell;
+
+/**
+ * Site\View\Cell\CommentsCell Test Case
+ */
+class CommentsCellTest extends TestCase
+{
+
+    /**
+     * Request mock
+     *
+     * @var \Cake\Http\ServerRequest|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $request;
+
+    /**
+     * Response mock
+     *
+     * @var \Cake\Http\Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $response;
+
+    /**
+     * Test subject
+     *
+     * @var \Site\View\Cell\CommentsCell
+     */
+    public $Comments;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->request = $this->getMockBuilder('Cake\Http\ServerRequest')->getMock();
+        $this->response = $this->getMockBuilder('Cake\Http\Response')->getMock();
+        $this->Comments = new CommentsCell($this->request, $this->response);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Comments);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test display method
+     *
+     * @return void
+     */
+    public function testDisplay()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
