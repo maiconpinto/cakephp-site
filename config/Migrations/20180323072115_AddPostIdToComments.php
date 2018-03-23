55958413a6adb43a7ef0fa93bb6fe5e1e315c55a
Maicon Pinto <maiconsilva.pinto@gmail.com>
2018-03-23 04:23:31 -0300

    Migrations - Change association Comments and Posts

---
+++ config/Migrations/20180323072115_AddPostIdToComments.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddPostIdToComments extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('comments');
+        $table->addColumn('post_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
