ca5f1a9b6ad01c054822a0fcf2bf2ea5e693dbd0
Maicon Pinto <maiconsilva.pinto@gmail.com>
2018-03-27 09:37:11 -0300

    PartnersCell

---
+++ tests/TestCase/View/Cell/PartnersCellTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Site\Test\TestCase\View\Cell;
+
+use Cake\TestSuite\TestCase;
+use Site\View\Cell\PartnersCell;
+
+/**
+ * Site\View\Cell\PartnersCell Test Case
+ */
+class PartnersCellTest extends TestCase
+{
+
+    /**
+     * Request mock
+     *
+     * @var \Cake\Http\ServerRequest|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $request;
+
+    /**
+     * Response mock
+     *
+     * @var \Cake\Http\Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $response;
+
+    /**
+     * Test subject
+     *
+     * @var \Site\View\Cell\PartnersCell
+     */
+    public $Partners;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->request = $this->getMockBuilder('Cake\Http\ServerRequest')->getMock();
+        $this->response = $this->getMockBuilder('Cake\Http\Response')->getMock();
+        $this->Partners = new PartnersCell($this->request, $this->response);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Partners);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test display method
+     *
+     * @return void
+     */
+    public function testDisplay()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
